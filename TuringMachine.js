896e393c8169b1b4b81cf439bc0b38e14df57536
Mauro Vogel <mavogel@student.ethz.ch>
2023-07-31 14:49:58 +0200

    playing around #1

--- TuringMachine.js
+++ TuringMachine.js
@@ -1,1 +1,14 @@
+//basic script to test things around (for now)
+
 console.log("Hello world");
+
+//changes color from orange to black & back when clicking on text
+function changeColor(event){
+    if(event.target.style.color === 'black'){
+        event.target.style.color = 'rgb(255, 168, 36)';
+    }
+    else{
+        event.target.style.color = 'black';
+    }
+}
+document.getElementById('head-title').onclick = changeColor;
